8b47d6576c6b8f7e1483840c29d3584ebb244717
Marco Picone <picone.m@gmail.com>
2024-01-25 17:44:02 +0100

    Initial code migration to wldt-core 0.3.0

--- src/main/java/it/wldt/adapter/mqtt/digital/MqttDigitalAdapter.java
+++ src/main/java/it/wldt/adapter/mqtt/digital/MqttDigitalAdapter.java
@@ -14,14 +14,32 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+/**
+ * The `MqttDigitalAdapter` class is a Digital Adapter implementation for MQTT communication. It extends the
+ * `DigitalAdapter` class and provides functionality to interact with a Digital Twin over MQTT. The adapter
+ * subscribes to incoming topics, handles state updates, and publishes events and property changes over MQTT.
+ *
+ * The class utilizes the Eclipse Paho MQTT client library for MQTT communication.
+ *
+ * @author Marco Picone, Ph.D. - picone.m@gmail.com, Marta Spadoni University of Bologna
+ */
 public class MqttDigitalAdapter extends DigitalAdapter<MqttDigitalAdapterConfiguration> {
     
     private final static Logger logger = LoggerFactory.getLogger(MqttDigitalAdapter.class);
     
     private final MqttClient mqttClient;
 
+    /**
+     * Constructs an instance of the `MqttDigitalAdapter` class with the specified identifier and configuration.
+     * It initializes the MQTT client with the provided broker connection details.
+     *
+     * @param id The unique identifier for the adapter.
+     * @param configuration The configuration for the MQTT Digital Adapter.
+     * @throws MqttException If there is an issue with the MQTT client initialization.
+     */
     public MqttDigitalAdapter(String id, MqttDigitalAdapterConfiguration configuration) throws MqttException {
         super(id, configuration);
         mqttClient = new MqttClient(getConfiguration().getBrokerConnectionString(),
@@ -29,66 +47,53 @@ public class MqttDigitalAdapter extends DigitalAdapter<MqttDigitalAdapterConfigu
                 getConfiguration().getPersistence());
     }
 
+    /**
+     * Callback method allowing the Digital Adapter to receive the updated Digital Twin State together with
+     * the previous state and the list of applied changes
+     *
+     * @param newDigitalTwinState The new Digital Twin State computed by the Shadowing Function
+     * @param previousDigitalTwinState The previous Digital Twin State
+     * @param digitalTwinStateChangeList The list of applied changes to compute the new Digital Twin State
+     */
     @Override
-    protected void onStateChangePropertyCreated(DigitalTwinStateProperty<?> digitalTwinStateProperty) {
+    protected void onStateUpdate(DigitalTwinState newDigitalTwinState, DigitalTwinState previousDigitalTwinState, ArrayList<DigitalTwinStateChange> digitalTwinStateChangeList) {
 
-    }
+        if (digitalTwinStateChangeList != null && !digitalTwinStateChangeList.isEmpty()) {
 
-    @Override
-    protected void onStateChangePropertyUpdated(DigitalTwinStateProperty<?> digitalTwinStateProperty) {
-        if(getConfiguration().getPropertyUpdateTopics().containsKey(digitalTwinStateProperty.getKey())){
-            PropertyOutgoingTopic<?> outgoingTopic = getConfiguration().getPropertyUpdateTopics().get(digitalTwinStateProperty.getKey());
-            publishOnDigitalTwinOutgoingTopic(outgoingTopic, outgoingTopic.applyPublishFunction(digitalTwinStateProperty));
+            // Iterate through each state change in the list
+            for (DigitalTwinStateChange stateChange : digitalTwinStateChangeList) {
+
+                // Get information from the state change
+                DigitalTwinStateChange.Operation operation = stateChange.getOperation();
+                DigitalTwinStateChange.ResourceType resourceType = stateChange.getResourceType();
+                DigitalTwinStateResource resource = stateChange.getResource();
+
+                // Search for property value variation
+                if((resourceType.equals(DigitalTwinStateChange.ResourceType.PROPERTY) || resourceType.equals(DigitalTwinStateChange.ResourceType.PROPERTY_VALUE)) &&
+                        operation.equals(DigitalTwinStateChange.Operation.OPERATION_UPDATE) && resource instanceof DigitalTwinStateProperty){
+
+                    DigitalTwinStateProperty<?> digitalTwinStateProperty = (DigitalTwinStateProperty<?>) resource;
+
+                    if(getConfiguration().getPropertyUpdateTopics().containsKey(digitalTwinStateProperty.getKey())){
+                        PropertyOutgoingTopic<?> outgoingTopic = getConfiguration().getPropertyUpdateTopics().get(digitalTwinStateProperty.getKey());
+                        publishOnDigitalTwinOutgoingTopic(outgoingTopic, outgoingTopic.applyPublishFunction(digitalTwinStateProperty));
+                    }
+                }
+            }
+        } else {
+            // No state changes
+            logger.info("No relevant DT's state changes detected !");
         }
     }
 
+    /**
+     * Callback method to receive a new computed Event Notification (associated to event declared in the DT State)
+     * In the case of the MQTT Digital Adapter, it sends the received event over MQTT on the configured topic.
+     *
+     * @param digitalTwinStateEventNotification The generated Notification associated to a DT Event
+     */
     @Override
-    protected void onStateChangePropertyDeleted(DigitalTwinStateProperty<?> digitalTwinStateProperty) {
-
-    }
-
-    @Override
-    protected void onStatePropertyUpdated(DigitalTwinStateProperty<?> digitalTwinStateProperty) {
-        logger.info("MQTT Digital Adapter({}) received property update", this.getId());
-    }
-
-    @Override
-    protected void onStatePropertyDeleted(DigitalTwinStateProperty<?> digitalTwinStateProperty) {
-
-    }
-
-    @Override
-    protected void onStateChangeActionEnabled(DigitalTwinStateAction digitalTwinStateAction) {
-
-    }
-
-    @Override
-    protected void onStateChangeActionUpdated(DigitalTwinStateAction digitalTwinStateAction) {
-
-    }
-
-    @Override
-    protected void onStateChangeActionDisabled(DigitalTwinStateAction digitalTwinStateAction) {
-
-    }
-
-    @Override
-    protected void onStateChangeEventRegistered(DigitalTwinStateEvent digitalTwinStateEvent) {
-
-    }
-
-    @Override
-    protected void onStateChangeEventRegistrationUpdated(DigitalTwinStateEvent digitalTwinStateEvent) {
-
-    }
-
-    @Override
-    protected void onStateChangeEventUnregistered(DigitalTwinStateEvent digitalTwinStateEvent) {
-
-    }
-
-    @Override
-    protected void onDigitalTwinStateEventNotificationReceived(DigitalTwinStateEventNotification<?> digitalTwinStateEventNotification) {
+    protected void onEventNotificationReceived(DigitalTwinStateEventNotification<?> digitalTwinStateEventNotification) {
         logger.info("MQTT Digital Adapter({}) - received event: {}", this.getId(), digitalTwinStateEventNotification.getDigitalEventKey());
         if(this.getConfiguration().getEventNotificationTopics().containsKey(digitalTwinStateEventNotification.getDigitalEventKey())){
             EventNotificationOutgoingTopic<?> outgoingTopic = getConfiguration().getEventNotificationTopics().get(digitalTwinStateEventNotification.getDigitalEventKey());
@@ -96,26 +101,9 @@ public class MqttDigitalAdapter extends DigitalAdapter<MqttDigitalAdapterConfigu
         }
     }
 
-    @Override
-    protected void onStateChangeRelationshipCreated(DigitalTwinStateRelationship<?> digitalTwinStateRelationship) {
-
-    }
-
-    @Override
-    protected void onStateChangeRelationshipInstanceCreated(DigitalTwinStateRelationshipInstance<?> digitalTwinStateRelationshipInstance) {
-
-    }
-
-    @Override
-    protected void onStateChangeRelationshipDeleted(DigitalTwinStateRelationship<?> digitalTwinStateRelationship) {
-
-    }
-
-    @Override
-    protected void onStateChangeRelationshipInstanceDeleted(DigitalTwinStateRelationshipInstance<?> digitalTwinStateRelationshipInstance) {
-
-    }
-
+    /**
+     * Callback to notify the adapter on its correct startup
+     */
     @Override
     public void onAdapterStart() {
         connectToMqttBroker();
@@ -123,6 +111,9 @@ public class MqttDigitalAdapter extends DigitalAdapter<MqttDigitalAdapterConfigu
         notifyDigitalAdapterBound();
     }
 
+    /**
+     * Callback to notify the adapter that has been stopped
+     */
     @Override
     public void onAdapterStop() {
         try {
@@ -132,17 +123,24 @@ public class MqttDigitalAdapter extends DigitalAdapter<MqttDigitalAdapterConfigu
         }
     }
 
+    /**
+     * DT Life Cycle notification that the DT is correctly on Sync.
+     * In this case the MQTT Digital Adapter observ the list of events for their variation with the aim to
+     * publish the associated value over MQTT.
+     *
+     * @param currentDigitalTwinState
+     */
     @Override
-    public void onDigitalTwinSync(IDigitalTwinState digitalTwinState) {
+    public void onDigitalTwinSync(DigitalTwinState currentDigitalTwinState) {
         try {
-//            digitalTwinState.getActionList().ifPresent(actions ->
+//            currentDigitalTwinState.getActionList().ifPresent(actions ->
 //                actions.stream()
 //                        .map(DigitalTwinStateAction::getKey)
 //                        .filter(a -> getConfiguration().getActionIncomingTopics().containsKey(a))
 //                        .map(a -> getConfiguration().getActionIncomingTopics().get(a))
 //                        .forEach(this::subscribeClientToDigitalTwinIncomingTopic));
 
-            digitalTwinState.getEventList()
+            currentDigitalTwinState.getEventList()
                     .map(events -> events.stream()
                             .map(DigitalTwinStateEvent::getKey)
                             .filter(key -> getConfiguration().getEventNotificationTopics().containsKey(key))
@@ -159,26 +157,42 @@ public class MqttDigitalAdapter extends DigitalAdapter<MqttDigitalAdapterConfigu
         }
     }
 
+    /**
+     * DT Life Cycle notification that the DT is currently Not Sync
+     * @param currentDigitalTwinState
+     */
     @Override
-    public void onDigitalTwinUnSync(IDigitalTwinState digitalTwinState) {
+    public void onDigitalTwinUnSync(DigitalTwinState currentDigitalTwinState) {
 
     }
 
+    /**
+     * DT Life Cycle notification that the DT has been created
+     */
     @Override
     public void onDigitalTwinCreate() {
 
     }
 
+    /**
+     * DT Life Cycle Notification that the DT has correctly Started
+     */
     @Override
     public void onDigitalTwinStart() {
 
     }
 
+    /**
+     * DT Life Cycle Notification that the DT has been stopped
+     */
     @Override
     public void onDigitalTwinStop() {
 
     }
 
+    /**
+     * DT Life Cycle Notification that the DT has destroyed
+     */
     @Override
     public void onDigitalTwinDestroy() {
 
--- src/test/java/it/wldt/adapter/mqtt/digital/TestMain.java
+++ src/test/java/it/wldt/adapter/mqtt/digital/TestMain.java
@@ -1,33 +1,51 @@
 package it.wldt.adapter.mqtt.digital;
 
-import it.wldt.adapter.mqtt.digital.exception.MqttDigitalAdapterConfigurationException;
 import it.wldt.adapter.mqtt.digital.topic.MqttQosLevel;
 import it.wldt.adapter.mqtt.digital.utils.DefaultShadowingFunction;
 import it.wldt.adapter.mqtt.digital.utils.DummyPhysicalAdapter;
 import it.wldt.adapter.mqtt.digital.utils.DummyPhysicalAdapterConfiguration;
-import it.wldt.core.engine.WldtEngine;
-import it.wldt.exception.EventBusException;
-import it.wldt.exception.ModelException;
-import it.wldt.exception.WldtConfigurationException;
-import it.wldt.exception.WldtRuntimeException;
-import org.eclipse.paho.client.mqttv3.MqttException;
+import it.wldt.core.engine.DigitalTwin;
+import it.wldt.core.engine.DigitalTwinEngine;
 
 public class TestMain {
-    public static void main(String[] args) throws WldtConfigurationException, ModelException, WldtRuntimeException, EventBusException, MqttDigitalAdapterConfigurationException, MqttException {
+    public static void main(String[] args) {
 
-        WldtEngine engine = new WldtEngine(new DefaultShadowingFunction(), "mqtt-digital-twin");
-        engine.addPhysicalAdapter(new DummyPhysicalAdapter("test-pa", new DummyPhysicalAdapterConfiguration(), true));
+        try {
 
-        MqttDigitalAdapterConfiguration configuration = MqttDigitalAdapterConfiguration.builder("127.0.0.1", 1883)
-                .addPropertyTopic("energy", "dummy/properties/energy", MqttQosLevel.MQTT_QOS_0, value -> String.valueOf(((Double)value).intValue()))
-                .addEventNotificationTopic("overheating", "dummy/events/overheating/notifications", MqttQosLevel.MQTT_QOS_0, Object::toString)
-                .addPropertyTopic("switch", "dummy/properties/switch", MqttQosLevel.MQTT_QOS_0, Object::toString)
-                .addActionTopic("switch_off", "app/actions/switch-off", msg -> "OFF")
-                .addActionTopic("switch_on", "app/actions/switch-on", msg -> "ON")
-                .build();
-        engine.addDigitalAdapter(new MqttDigitalAdapter("test-da", configuration));
+            DigitalTwin digitalTwin = new DigitalTwin("mqtt-digital-twin", new DefaultShadowingFunction());
 
-        engine.startLifeCycle();
+            // Create and assign a Demo Physical Adapter generating random physical variation to test the MQTT Digital Adapter
+            digitalTwin.addPhysicalAdapter(
+                    new DummyPhysicalAdapter(
+                            "test-pa",
+                            new DummyPhysicalAdapterConfiguration(),
+                            true)
+            );
+
+            // Build the MQTT Digital Adapter Configuration
+            MqttDigitalAdapterConfiguration configuration = MqttDigitalAdapterConfiguration.builder("127.0.0.1", 1883)
+                    .addPropertyTopic("energy", "dummy/properties/energy", MqttQosLevel.MQTT_QOS_0, value -> String.valueOf(((Double)value).intValue()))
+                    .addEventNotificationTopic("overheating", "dummy/events/overheating/notifications", MqttQosLevel.MQTT_QOS_0, Object::toString)
+                    .addPropertyTopic("switch", "dummy/properties/switch", MqttQosLevel.MQTT_QOS_0, Object::toString)
+                    .addActionTopic("switch_off", "app/actions/switch-off", msg -> "OFF")
+                    .addActionTopic("switch_on", "app/actions/switch-on", msg -> "ON")
+                    .build();
+
+            // Add the MQTT Digital Adapter to the target Digital Twin
+            digitalTwin.addDigitalAdapter(new MqttDigitalAdapter("test-da", configuration));
+
+            // Create the Digital Twin Engine to execute the created DT instance
+            DigitalTwinEngine digitalTwinEngine = new DigitalTwinEngine();
+
+            // Add the Digital Twin to the Engine
+            digitalTwinEngine.addDigitalTwin(digitalTwin);
+
+            // Start all the DTs registered on the engine
+            digitalTwinEngine.startAll();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
